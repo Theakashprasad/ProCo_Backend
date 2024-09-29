5f656e09d6dd9425eb7346d3c56d9d2301b2bf5c
akashh <akashyoungstar123@gmail.com>
2024-09-30 02:13:31 +0530

    new4

--- dist/entities/payment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Payment = void 0;
-class Payment {
-    constructor(userId) {
-        this.userId = userId;
-    }
-}
-exports.Payment = Payment;
